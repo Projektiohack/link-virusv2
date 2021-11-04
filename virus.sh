400b16b932cf777e1343525cfae6e63feee75fc9
Projektiohack <93693938+Projektiohack@users.noreply.github.com>
2021-11-05 06:38:39 +0700

    Add files via upload

---
+++ virus.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+# copyrihgt by Fauzan Anugrah
+
+# variabel
+e=echo
+s=sleep
+v=https
+z=bit.ly
+k=3ild93L
+
+# pembersih texs
+c=clear
+$c # pembersih
+
+# tampilan
+$s 2
+figlet LINK VIRUS
+$s 1
+$e "=================================================="
+$e " {•} Author : Fauzan Anugrah"
+$e " {•} Team   : HACKER ANONYMOUS"
+$e "=================================================="
+$s 1
+# daftar menu
+echo         "<•> Welcome To Virus Fauzan <•>"
+echo "================================================"
+echo "{1} Link Virus"
+echo "{x} Keluar !!!"
+echo "================================================"
+read -p "{•} Pilih: " option;
+# data base
